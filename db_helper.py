12da4f1c709f149b6d99a20116287a844a2dafb3
robsonklima <robsonklima@gmail.com>
2019-08-09 07:34:44 -0300

    REF

--- db_helper.py
+++ db_helper.py
@@ -9,19 +9,29 @@ class DBHelper:
         self.db = "nhatos_v2"
 
     def __connect__(self):
-        self.con = pymysql.connect(host=self.host, user=self.user, password=self.password,
-                                   db=self.db, cursorclass=pymysql.cursors.DictCursor)
-        self.cur = self.con.cursor()
+        try:
+            self.con = pymysql.connect(host=self.host, user=self.user, password=self.password,
+                                       db=self.db, cursorclass=pymysql.cursors.DictCursor)
+            self.cur = self.con.cursor()
+        except MySQLError as ex:
+            print(ex)
 
     def __disconnect__(self):
-        self.con.close()
+        try:
+            self.con.close()
+        except MySQLError as ex:
+            print(ex)
 
     def fetch(self, sql):
-        self.__connect__()
-        self.cur.execute(sql)
-        result = self.cur.fetchall()
-        self.__disconnect__()
-        return result
+        try:
+            self.__connect__()
+            self.cur.execute(sql)
+            result = self.cur.fetchall()
+            self.__disconnect__()
+
+            return result
+        except MySQLError as ex:
+            print(ex)
 
     def execute(self, sql):
         try:
--- recommendation.py
+++ recommendation.py
@@ -4,6 +4,9 @@ from db_helper import DBHelper
 from operator import attrgetter
 
 
+def get_all_projects():
+    return DBHelper().fetch(u'SELECT * FROM projects;')
+
 def get_project_by_id(id):
     projects = DBHelper().fetch(u"SELECT * FROM projects WHERE id=%s;" % (id))
 
@@ -20,11 +23,20 @@ def get_project_by_rand():
 
     return None
 
-def get_all_projects():
-    return DBHelper().fetch(u'SELECT * FROM projects;')
-
 def get_projects_by_domain(domain):
     return DBHelper().fetch(u"SELECT * FROM projects WHERE domain='%s' ORDER BY added;" % (domain))
+
+def get_projects_non_processed(distance, sample, steps):
+    return DBHelper().fetch(u' SELECT   p.*' 
+                            u' FROM     projects p' 
+                            u' WHERE    p.id NOT IN(' 
+                            u'              SELECT project_id' 
+                            u'              FROM   recommendations' 
+                            u'              WHERE  CAST(distance AS DECIMAL(5,1)) = %s' 
+                            u'              AND    CAST(sample AS DECIMAL(5,1)) = %s' 
+                            u'              AND	   CAST(steps AS DECIMAL(5,1)) = %s' 
+                            u'          )'
+                            u' ORDER BY p.id ASC' % (distance, sample, steps))
 
 def get_requirements_by_code(code):
     return DBHelper().fetch(u"SELECT * FROM requirements WHERE code='%s';" % (code))
@@ -39,15 +51,15 @@ def get_requirements_distance(req_a_id, req_b_id):
 
 def insert_recommendation(project_id, requirement_id, base_date, distance, sample, steps):
     DBHelper().execute(u"INSERT INTO recommendations (project_id, requirement_id, base_date, distance, sample, steps) "
-                       u"VALUES (%s, %s, '%s', %s, %s);" % (project_id, requirement_id, base_date, distance, sample, steps))
+                       u"VALUES (%s, %s, '%s', %s, %s, %s);" % (project_id, requirement_id, base_date, distance, sample, steps))
 
 def delete_all_recommendations():
     DBHelper().execute(u"TRUNCATE TABLE recommendations;")
 
 
 distance, sample, steps, counter = 0.3, 0.7, 3, 0
-delete_all_recommendations()
-projects = get_all_projects()
+#delete_all_recommendations()
+projects = get_projects_non_processed(distance, sample, steps)
 
 for i, prj in enumerate(projects):
     requirements = get_requirements_by_code(prj['code'])
