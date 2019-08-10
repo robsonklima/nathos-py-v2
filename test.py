2e6a216dbd1edf6b16b42b154dae36b17ab5b7c0
robsonklima <robsonklima@gmail.com>
2019-08-09 22:45:35 -0300

    REF

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-
-print(u'Olá %s e %s' % (1, 2))
--- translate.py
+++ translate.py
@@ -2,18 +2,35 @@ from google.cloud import translate
 from db_helper import DBHelper
 
 
-projects = DBHelper().fetch(u'SELECT * FROM projects WHERE description_en IS NULL;')
+def get_all_projects():
+    return DBHelper().fetch(u'SELECT * FROM projects WHERE description_en IS NULL;')
+
+def get_all_requirements():
+    return DBHelper().fetch(u'SELECT * FROM requirements WHERE description_en IS NULL;')
+
+def update_project(translated_text, project_id):
+    DBHelper.execute(u" UPDATE projects "
+                     u" SET description_en='%s' WHERE id=%s;"
+                     % (translated_text, project_id))
+
+def update_requirement(translated_text, requirement_id):
+    DBHelper.execute(u" UPDATE requirements "
+                     u" SET description_en='%s' WHERE id=%s;"
+                     % (translated_text, requirement_id))
+
+
+projects = get_all_projects()
 
 for i, p in enumerate(projects):
     translate_client = translate.Client()
     translation = translate_client.translate(p['description'], target_language='en')
-    DBHelper.execute(u"UPDATE projects SET description_en='%s' WHERE id=%s;" %
-                     (translation['translatedText'], p['id']))
+    update_project(translation['translatedText'], p['id'])
 
-requirements = DBHelper().fetch(u'SELECT * FROM requirements WHERE description_en IS NULL;')
+requirements = get_all_requirements()
 
 for i, r in enumerate(requirements):
     translate_client = translate.Client()
     translation = translate_client.translate(r['description'], target_language='en')
-    DBHelper.execute(u"UPDATE requirements SET description_en='%s' WHERE id=%s;" %
-                     (translation['translatedText'], r['id']))
+    update_requirement(translation['translatedText'], r['id'])
+
+
